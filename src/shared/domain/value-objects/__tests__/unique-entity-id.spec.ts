32c7725a46a1ae6cd1f74e44a530602e3967505e
Matheus Mota <matheus.mota@eumedicoresidente.com.br>
2024-04-14 21:24:14 -0300

    Fix import statement for InvalidUuidError in UniqueEntityId class

--- src/shared/domain/value-objects/__tests__/unique-entity-id.spec.ts
+++ src/shared/domain/value-objects/__tests__/unique-entity-id.spec.ts
@@ -1,5 +1,5 @@
 import { validate as validateUUID } from "uuid";
-import { InvalidUuidError } from "./../../../errors/invalid-uuid.error";
+import InvalidUuidError from "./../../../errors/invalid-uuid.error";
 import { UniqueEntityId } from "./../unique-entity-id";
 
 describe("UniqueEntityID", () => {
