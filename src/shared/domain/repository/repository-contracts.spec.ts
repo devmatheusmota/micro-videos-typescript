e4e1a8a058ca9f33cf0ece1d4da4f8a9ab370a1f
Matheus Mota <matheus.mota@eumedicoresidente.com.br>
2024-06-26 07:56:23 -0300

    ✅ (repository-contracts.spec.ts): add unit tests for SearchParams and SearchResult classes ✨ (repository-contracts.ts): add SearchResult class to encapsulate search results ♻️ (repository-contracts.ts): fix sort_dir setter to check for sort property instead of sort_dir

--- src/shared/domain/repository/repository-contracts.spec.ts
+++ src/shared/domain/repository/repository-contracts.spec.ts
@@ -1,9 +1,7 @@
-import { SearchParams } from "./repository-contracts";
+import { SearchParams, SearchResult } from "./repository-contracts";
 
 describe("Search Params Unit Tests", () => {
   test("page prop", () => {
-    const params = new SearchParams();
-
     const arranges: { page: any; expected: number }[] = [
       { page: null, expected: 1 },
       { page: undefined, expected: 1 },
@@ -27,8 +25,6 @@ describe("Search Params Unit Tests", () => {
   });
 
   test("per_page prop", () => {
-    const params = new SearchParams();
-
     const arranges: { per_page: any; expected: number }[] = [
       { per_page: null, expected: 15 },
       { per_page: undefined, expected: 15 },
@@ -49,5 +45,160 @@ describe("Search Params Unit Tests", () => {
         arrange.expected
       );
     }
+  });
+
+  test("sort prop", () => {
+    const arranges: { sort: any; expected: string }[] = [
+      { sort: null, expected: null },
+      { sort: undefined, expected: null },
+      { sort: "", expected: null },
+      { sort: "fake", expected: "fake" },
+      { sort: 0, expected: "0" },
+      { sort: -1, expected: "-1" },
+      { sort: 5.5, expected: "5.5" },
+      { sort: true, expected: "true" },
+      { sort: false, expected: "false" },
+      { sort: {}, expected: "[object Object]" },
+      { sort: 1, expected: "1" },
+      { sort: 2, expected: "2" },
+    ];
+
+    for (const arrange of arranges) {
+      expect(new SearchParams({ sort: arrange.sort }).sort).toBe(
+        arrange.expected
+      );
+    }
+  });
+
+  test("sort_dir prop", () => {
+    let params = new SearchParams();
+    expect(params.sort_dir).toBe(null);
+
+    params = new SearchParams({ sort: null });
+    expect(params.sort_dir).toBe(null);
+
+    params = new SearchParams({ sort: undefined });
+    expect(params.sort_dir).toBe(null);
+
+    params = new SearchParams({ sort: "" });
+    expect(params.sort_dir).toBe(null);
+
+    const arranges: { sort_dir: any; expected: string }[] = [
+      { sort_dir: null, expected: "asc" },
+      { sort_dir: undefined, expected: "asc" },
+      { sort_dir: "", expected: "asc" },
+      { sort_dir: 0, expected: "asc" },
+      { sort_dir: "fake", expected: "asc" },
+      { sort_dir: "asc", expected: "asc" },
+      { sort_dir: "ASC", expected: "asc" },
+      { sort_dir: "desc", expected: "desc" },
+      { sort_dir: "DESC", expected: "desc" },
+    ];
+
+    for (const arrange of arranges) {
+      expect(
+        new SearchParams({ sort: "field", sort_dir: arrange.sort_dir }).sort_dir
+      ).toBe(arrange.expected);
+    }
+  });
+
+  test("filter prop", () => {
+    const params = new SearchParams();
+    expect(params.filter).toBe(null);
+
+    const arranges: { filter: any; expected: any }[] = [
+      { filter: null, expected: null },
+      { filter: undefined, expected: null },
+      { filter: "", expected: null },
+      { filter: "fake", expected: "fake" },
+      { filter: 0, expected: "0" },
+      { filter: -1, expected: "-1" },
+      { filter: 5.5, expected: "5.5" },
+      { filter: true, expected: "true" },
+      { filter: false, expected: "false" },
+      { filter: {}, expected: "[object Object]" },
+      { filter: 1, expected: "1" },
+      { filter: 2, expected: "2" },
+    ];
+
+    for (const arrange of arranges) {
+      expect(new SearchParams({ filter: arrange.filter }).filter).toBe(
+        arrange.expected
+      );
+    }
+  });
+});
+
+describe("Search Result Unit Tests", () => {
+  test("constructor props", () => {
+    let result = new SearchResult({
+      items: ["entity1", "entity2"] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      filter: null,
+      sort: null,
+      sort_dir: null,
+    });
+
+    expect(result.toJSON()).toStrictEqual({
+      items: ["entity1", "entity2"],
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+      filter: null,
+      sort: null,
+      sort_dir: null,
+    });
+
+    result = new SearchResult({
+      items: ["entity1", "entity2"] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      filter: "test",
+      sort: "name",
+      sort_dir: "asc",
+    });
+
+    expect(result.toJSON()).toStrictEqual({
+      items: ["entity1", "entity2"],
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+      filter: "test",
+      sort: "name",
+      sort_dir: "asc",
+    });
+  });
+
+  it("should set last_page = 1 when per_page is greater than total", () => {
+    const result = new SearchResult({
+      items: [],
+      total: 4,
+      current_page: 2,
+      per_page: 15,
+      filter: null,
+      sort: null,
+      sort_dir: null,
+    });
+
+    expect(result.last_page).toBe(1);
+  });
+
+  test("last_page prop when total is not a multiple of per_page", () => {
+    const result = new SearchResult({
+      items: [],
+      total: 101,
+      current_page: 7,
+      per_page: 20,
+      filter: null,
+      sort: null,
+      sort_dir: null,
+    });
+
+    expect(result.last_page).toBe(6);
   });
 });
--- src/shared/domain/repository/repository-contracts.ts
+++ src/shared/domain/repository/repository-contracts.ts
@@ -72,12 +72,12 @@ export class SearchParams {
   }
 
   private set sort_dir(value: string) {
-    if (!this._sort_dir) {
+    if (!this._sort) {
       this._sort_dir = null;
       return;
     }
 
-    const dir = `${value.toLowerCase()}`;
+    const dir = `${value}`.toLowerCase();
     this._sort_dir = dir !== "asc" && dir !== "desc" ? "asc" : dir;
   }
 
@@ -91,10 +91,56 @@ export class SearchParams {
   }
 }
 
+type SearchResultProps<E extends Entity, Filter> = {
+  readonly items: E[];
+  readonly total: number;
+  readonly current_page: number;
+  readonly per_page: number;
+  readonly sort: string | null;
+  readonly sort_dir: string | null;
+  readonly filter: Filter;
+};
+
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly current_page: number;
+  readonly per_page: number;
+  readonly last_page: number;
+  readonly sort: string | null;
+  readonly sort_dir: string | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.last_page = Math.ceil(this.total / this.per_page);
+    this.sort = props.sort;
+    this.sort_dir = props.sort_dir;
+    this.filter = props.filter;
+  }
+
+  toJSON() {
+    return {
+      items: this.items,
+      total: this.total,
+      current_page: this.current_page,
+      per_page: this.per_page,
+      last_page: Math.ceil(this.total / this.per_page),
+      sort: this.sort,
+      sort_dir: this.sort_dir,
+      filter: this.filter,
+    };
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchOutput,
-  SearchInput = SearchParams
+  Filter = string,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult<E, Filter>
 > extends RepositoryInterface<E> {
   search(props: SearchInput): Promise<SearchOutput>;
 }
